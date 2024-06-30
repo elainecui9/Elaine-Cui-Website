05220d4490d2dcb4dfaf692e4c8a37bad4060880
elainecui9 <elaine.cui@gmail.com>
2024-06-30 17:31:25 -0500

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Elaine Cui',
   description: 'Portfolio',
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
@@ -16,9 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
